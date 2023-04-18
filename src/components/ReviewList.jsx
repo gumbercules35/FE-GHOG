a6862daa7b2f092b899b260d003333d5f2555202
Matt Walden <mattwalden@gmail.com>
2023-04-18 14:56:03 +0100

    added sorting to reviews

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -2,28 +2,44 @@ import { useEffect, useState } from "react";
 import * as api from "../api";
 import ReviewCard from "./ReviewCard";
 import { useSearchParams } from "react-router-dom";
+import SortReviews from "./SortReviews";
 
 export default function ReviewList() {
   const [reviewList, setReviewList] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [searchParams, setSearchParams] = useSearchParams();
   const categoryQuery = searchParams.get("category");
+  const orderQuery = searchParams.get("order");
+  const sortQuery = searchParams.get("sort_by");
 
   useEffect(() => {
     setIsLoading(true);
-    api.getReviews(categoryQuery).then((reviews) => {
+    api.getReviews(categoryQuery, orderQuery, sortQuery).then((reviews) => {
       setReviewList(reviews);
       setIsLoading(false);
     });
-  }, [categoryQuery]);
+  }, [categoryQuery, orderQuery, sortQuery]);
   return isLoading ? (
     <p>Loading!</p>
   ) : (
     <main className="Content">
+      <SortReviews
+        setSearchParams={setSearchParams}
+        setIsLoading={setIsLoading}
+        searchParams={searchParams}
+      />
+
       {categoryQuery ? (
         <h2>Showing {categoryQuery} Reviews</h2>
       ) : (
         <h2>Showing All Reviews</h2>
+      )}
+      {orderQuery || sortQuery ? (
+        <h3>
+          Sorted by {sortQuery} in {orderQuery} Order
+        </h3>
+      ) : (
+        <h3>Sorted by Date Created in Descending Order</h3>
       )}
       <ul id="reviewList">
         {reviewList.map((review) => {
